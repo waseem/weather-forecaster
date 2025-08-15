041aa5a22866a5d9f3db1baf8537466b9fa0fc6f
Waseem Ahmad <talk.to.waseem@gmail.com>
2025-08-15 14:46:25 -0600

    Refactor Geocoding service

--- app/services/geocoding/service.rb
+++ app/services/geocoding/service.rb
@@ -12,13 +12,17 @@ module Geocoding
         raise EmptyResponseError.new("Could not geocode address. Please enter a valid address.") if response.blank?
 
         geocoded = response.first
+        validate_response!(geocoded)
+        geocoded
+      end
 
+      private
+
+      def validate_response!(geocoded)
         raise MissingLatitudeError.new("Could not determine the latitude of address.") if geocoded.latitude.blank?
         raise MissingLongitudeError.new("Could not determine the longitude of address.") if geocoded.longitude.blank?
         raise MissingCountryCode.new("Could not determine the country code of address.") if geocoded.country_code.blank?
         raise MissingPostalCodeError.new("Could not determine the postal code of address.") if geocoded.postal_code.blank?
-
-        geocoded
       end
     end
   end
